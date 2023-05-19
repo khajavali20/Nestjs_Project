2730e20d72c895d8534f51b78fa87f7a25d9b8d2
khajavali20 <sayyed.khajavali1234@gmail.com>
2023-05-19 15:36:41 +0530

    modified

--- app/e-commerce/src/app.module.ts
+++ app/e-commerce/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    PaymentsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI, { autoIndex: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
