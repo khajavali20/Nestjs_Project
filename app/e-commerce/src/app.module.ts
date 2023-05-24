d9e2066add7560140dd38e173930812d4ae3748b
khajavali20 <sayyed.khajavali1234@gmail.com>
2023-05-24 19:18:37 +0530

    authorization module created

--- app/e-commerce/src/app.module.ts
+++ app/e-commerce/src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { StripeModule } from './stripe/stripe.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { StripeModule } from './stripe/stripe.module';
     MongooseModule.forRoot(process.env.MONGO_DB_URI, { autoIndex: true }),
     UserModule,
     StripeModule.forRoot(process.env.STRIPE_KEY, { apiVersion: '2022-11-15' }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
